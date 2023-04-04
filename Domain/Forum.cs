41d085b4f37fd7ed350a1681df53dd63e0b2fd46
iamsu <iamsuhani5@gmail.com>
2023-04-04 18:26:36 +0200

    started forum part..

---
+++ Domain/Forum.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain;
+
+public class Forum
+{
+    public string  Title { get; set; }
+    public string  Description { get; set; }
+    public DateTime  DateTime { get; set; }
+
+    public Forum()
+    {
+    }
+
+    public Forum(string title, string description, DateTime dateTime)
+    {
+        Title = title;
+        Description = description;
+        DateTime = dateTime;
+    }
+}
---
+++ Domain/Post.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain;
+
+public class Post
+{
+    public string  PostTitle { get; set; }
+    public string  Body { get; set; }
+    public DateTime DateTime { get; set; }
+    public string  CreatedBy { get; set; }
+
+    public Post()
+    {
+    }
+
+    public Post(string postTitle, string body, DateTime dateTime, string createdBy)
+    {
+        PostTitle = postTitle;
+        Body = body;
+        DateTime = dateTime;
+        CreatedBy = createdBy;
+    }
+}
